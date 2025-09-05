bffa68ce7a103848eb23b0109e898d7b2e6eea3d
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-09-05 22:26:56 +0530

    inital commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const app = express();
+
+//req, res => request and response
+app.get('/', function(req, res){
+    res.send('Hello World');
+})      
+
+app.listen(3000);
