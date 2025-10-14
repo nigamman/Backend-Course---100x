a04c51b8b341d138255658efb664e046fff154d1
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-10-14 14:53:45 +0530

    add midleware

--- week3/middleware2.js
+++ week3/middleware2.js
@@ -1,34 +1,28 @@
 const express = require('express');
 const app = express();
 
-//function that returns a boolean if the age of the person is more that 14
-function isOldEnough(age) {
+//using middleware 
+function isOldEnough(req, res, next) {
+    const age = req.query.age;
     if(age >= 14) {
-        return true;
-    } else {
-        return false;
+        next();
+    } else{
+        res.json({
+            msg: "Sorry you are not of age yet."
+        });
     }
 }
-app.get("/ride1", function(req,res) {
-    if(isOldEnough(req.query.age)) {
-        res.json({
-        msg: "You have successfully riden the ride 1"
-    });     
-    } else {
-        res.status(411).json({
-            msg: "Sorry you are not of age yet"
-        });
-    }
+
+app.get("/ride3", function(req,res) {
+    res.json({
+        msg: "You have successfully riden the ride 3."
+    })
 });
-app.get("/ride2", function(req,res) {
-    if(isOldEnough(req.query.age)) {
-        res.json({
-        msg: "You have successfully riden the ride 1"
-    });     
-    } else {
-        res.status(411).json({
-            msg: "Sorry you are not of age yet"
-        });
-    }
+
+app.get("/ride4", function(req,res){
+    res.json({
+        msg: "You have successfully riden the ride 4"
+    })
 });
+
 app.listen(3001);
