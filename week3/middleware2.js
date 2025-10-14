40f9665c2f6fdc074a97dfd444e64b2f07aaf871
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-10-14 14:33:26 +0530

    add midlleware 2

---
+++ week3/middleware2.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+
+//function that returns a boolean if the age of the person is more that 14
+function isOldEnough(age) {
+    if(age >= 14) {
+        return true;
+    } else {
+        return false;
+    }
+}
+app.get("/ride1", function(req,res) {
+    if(isOldEnough(req.query.age)) {
+        res.json({
+        msg: "You have successfully riden the ride 1"
+    });     
+    } else {
+        res.status(411).json({
+            msg: "Sorry you are not of age yet"
+        });
+    }
+});
+
+app.listen(3001);
