581a5ea64545c6ca5ab044388a6604b1cf549f10
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-11-07 20:35:57 +0530

    add asignment

---
+++ mongodb-assignment/db/index.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+
+//connect ot mongoo db
+mongoose.connect('mongodb://');
+
+//define schema
+const AdminSchema = new mongoose.AdminSchema({
+    username: String,
+    password: String
+});
+const UserSchema = new mongoose.UserSchema({
+    username: String,
+    password: String,
+    purchasedCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
+});
+const CourseSchema = new mongoose.Schema({
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
+});
+const Admin = mongoose.model('Admin', AdminSchema);
+const User = mongoose.model('User', UserSchema);
+const Course = mongoose.model('Course', CourseSchema);
+
+module.exports = {
+    Admin, 
+    User,
+    Course
+}
---
+++ mongodb-assignment/middleware/admin.js
@@ -0,0 +1,22 @@
+const { Admin } = require("../db");
+
+//Middleware for handling 
+function adminMiddleware(req,res,next) {
+    const username = req.headers.username;
+    const password = req.headers.password;
+
+    Admin.findOne({
+        username: username,
+        password: password
+    })
+    .then(function(value) {
+        if(value) {
+            next();
+        } else {
+            res.status(403).json({
+                msg: "Admin doesnt exist"
+            })
+        }
+    })
+}
+module.exports = adminMiddleware;
