112d94b897ce53a201d290ccc223a656401021b4
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-11-11 16:37:25 +0530

    add components

--- week5-react-foundation/counter-app/src/App.jsx
+++ week5-react-foundation/counter-app/src/App.jsx
@@ -3,14 +3,26 @@ import {useState} from "react";
 function App() {
   const [count, setCount] = useState(0);
 
-  function onClickHandler() {
-    setCount(count + 1);
-  }
-
   return (
     <div>
-      <button onClick={onClickHandler}> Counter {count} </button>
+      <CustomButton count={count} setCount= {setCount}></CustomButton>
+      <CustomButton count={count + 10} setCount= {setCount}></CustomButton>
+      <CustomButton count={count * 55} setCount= {setCount}></CustomButton>
+      <CustomButton count={count - 21} setCount= {setCount}></CustomButton>
     </div>  
   )
 }
+
+//component
+function CustomButton(props) {
+
+  function onClickHandler() {
+    props.setCount(props.count + 1);
+  }
+
+  return <button onClick={onClickHandler}>
+    Counter {props.count}
+  </button>
+}
+
 export default App
