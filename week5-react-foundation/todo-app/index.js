53e03999b92492f5a487eba0970079990e761d65
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-11-15 22:15:56 +0530

    correct

--- week5-react-foundation/todo-app/index.js
+++ week5-react-foundation/todo-app/index.js
@@ -1,17 +1,18 @@
 const express = require("express");
-const { createTodo } = require("./types");
-const app = express();
+const { createTodo, updateTodo } = require("./types");
+const { todo } = require("./db");
 
+const app = express();
 app.use(express.json()); //middleware
 
 app.post("/todo",async function(req, res) {
     const createPayload = req.body;
     const parsedPayload = createTodo.safeParse(createPayload);
+
     if(!parsedPayload.success) {
         res.status(411).json({
             msg: "You sent the wrong inputs."
         })
-        return;
     }
     //put it in mongoodb  
     await todo.create({
@@ -30,25 +31,27 @@ app.get("/todos", async function(req, res) {
 
     res.json({
         todos
-    })
+    });
 
-})
+});
 
 app.put("/completed", async function(req,res) {
     const updatePayload = req.body;
     const parsedPayload = updateTodo.safeParse(updatePayload);
+
     if(!parsedPayload.success) {
         res.status(411).json({
             msg: "You sent the wrong inputs",
-        })
-        return;
+        });
     }
     await todo.update({
         _id: req.body.id
     }, {
         completed: true
-    })
+    });
     res.json({
         msg: "todo marked as completed"
     })
 })
+
+app.listen(3000, () => console.log("Server running on 3000"));
