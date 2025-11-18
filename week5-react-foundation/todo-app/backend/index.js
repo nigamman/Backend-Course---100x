fd8c42786d33ee58b08a2219b610b1a9d652b6ec
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-11-18 16:30:58 +0530

    update todo app

--- week5-react-foundation/todo-app/backend/index.js
+++ week5-react-foundation/todo-app/backend/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const { createTodo, updateTodo } = require("./types");
 const { todo } = require("./db");
-
+const cors = require("cors");
 const app = express();
+
 app.use(express.json()); //middleware
+app.use(cors());
 
 app.post("/todo",async function(req, res) {
     const createPayload = req.body;
@@ -13,6 +15,7 @@ app.post("/todo",async function(req, res) {
         res.status(411).json({
             msg: "You sent the wrong inputs."
         })
+        return;
     }
     //put it in mongoodb  
     await todo.create({
@@ -27,7 +30,7 @@ app.post("/todo",async function(req, res) {
 })
 
 app.get("/todos", async function(req, res) {
-    const todos = await todo.find({});
+    //const todos = await todo.find({});
 
     res.json({
         todos
@@ -43,6 +46,7 @@ app.put("/completed", async function(req,res) {
         res.status(411).json({
             msg: "You sent the wrong inputs",
         });
+        return;
     }
     await todo.update({
         _id: req.body.id
