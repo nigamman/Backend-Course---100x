95e0842c332e8ef57715e169b147b97155bcf0d3
Shivansh Nigam <shivanshnigam07@gmail.com>
2025-11-19 18:12:18 +0530

    add

--- week6-react-2/react-hooks/src/App.jsx
+++ week6-react-2/react-hooks/src/App.jsx
@@ -1,10 +1,26 @@
-function App() {
+import React, {useState} from "react"
+
+function App() { 
+  const [title, setTitle] = useState("I am admin");
+
+  function updateTitle() {
+    setTitle("my name is "+ Math.random());
+  }
 
   return (
     <div>
-
+      <button onClick={updateTitle}>Update the title</button>
+      <Header title={title}></Header> {/* dynamic header */}
+      <br></br>
+      <Header title= "heading-2"></Header>{/* static header */}
+      <Header title= "heading-3"></Header>
     </div>
-  )
+  )                                                         
 }
+const Header = React.memo(function Header({title}) {
+  return <div>
+    {title}
+  </div>
+})
 
-export default App
+export default App;
