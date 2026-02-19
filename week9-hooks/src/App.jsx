8718cbede9cb7124d0fe7908eccb6d608bde8bbd
Shivansh Nigam <shivanshnigam07@gmail.com>
2026-02-20 02:02:08 +0530

    feat: add MyComponent with increment functionality

--- week9-hooks/src/App.jsx
+++ week9-hooks/src/App.jsx
@@ -1,12 +1,28 @@
 import './App.css'
+import { useState } from 'react'
 
 function App() {
 
   return (
     <>
-    
+      <MyComponent />
     </>
   )
+}
+
+function MyComponent() {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <p>{count}</p>
+      <button onClick={incrementCount}>Increment</button>
+    </div>  
+  );
 }
 
 export default App
